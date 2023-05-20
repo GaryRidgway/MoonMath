d6e97e00454ddb2ad19346f53f4afdcbcc76b0f4
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-05-20 17:43:02 -0500

    Smoothing out inheritance.

--- src/js/components/NCircle.js
+++ src/js/components/NCircle.js
@@ -1,3 +1,19 @@
-class NCircle extends PSVG() {
+class NCircle extends PSVG {
+    constructor() {
+        super();
 
+        this.config.merge({
+            tag: 'circle',
+            'stroke-width' : 2,
+            r: 0,
+            cx: this.config.x,
+            cy: this.config.y
+        });
+
+        this.config.merge.apply(this.config, arguments);
+    }
+
+    draw() {
+        super.draw();
+    }
 }
--- src/js/components/PSVG.js
+++ src/js/components/PSVG.js
@@ -1,30 +1,28 @@
 class PSVG {
     constructor() {
-        this.config = new CNF(arguments);
-        this.config.id = ID();
+        this.config = new CNF();
+        this.config.merge.apply(this.config, arguments);
     }
 
     draw() {
-        if (!document.querySelector('#psvg-id-' + this.config.id)) {
-            this.config.svgc_selector.innerHTML += this.initHtml();
-        }
-    }
-
-    initHtml() {
         let entries = Object.keys(this.config);
 
-        let html = ['<' + this.config['tag']];
+        let element = document.querySelector('#' + this.config.id);
+        if(!element) {
+            element = document.createElement(this.config['tag']);
+            console.log(this.config);
+            this.config['svgc-selector'].append(element);
+        }
+
         entries.splice(entries.indexOf('tag'), 1);
 
         for (let i = entries.length-1; i >= 0 ; i--) {
-            if(typeof this.config[entries[i].toString()] !== 'object') {
-                html.push(entries[i] + '="' + this.config[entries[i]] + '"');
+            const entry = this.config[entries[i].toString()];
+            if(typeof entry !== 'object' && entry !== 'none' && entry !== null) {
+                element.setAttribute(entries[i], this.config[entries[i]]);
             }
             
             entries.splice(i, 1);
         }
-
-        html = html.join(' ') + '/>';
-        return html;
     }
 }
